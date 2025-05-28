7ef88b9954a4b3a438c2f486dc33c5e944c477ed
Elisa Holland <102695327+emholland@users.noreply.github.com>
2025-05-28 11:25:58 -0400

    Homepage done besides tagline

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,17 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Home from './Home';
+import HomeT from './Pages/HomeT'; // if you want to use the Home2 component
 import Articles from './Pages/Articles'; // make sure path matches
 import AuthorInfo from './Pages/AuthorInfo';
 import ResourcePage from './Pages/Resources';
 import './index.css'; // your global styles
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<HomeT />} />
         <Route path="/articles" element={<Articles />} />
         <Route path="/authorinfo" element={<AuthorInfo />} />
         <Route path="/resources" element={<ResourcePage />} />
