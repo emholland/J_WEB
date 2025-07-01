b7b2c3ea597202a4d6fc88cb47875fda21c3f12c
Elisa Holland <102695327+emholland@users.noreply.github.com>
2025-07-01 15:27:48 -0400

    cors origin added

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -106,6 +106,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "https://j-web.onrender.com",
+    "https://mantisreview.onrender.com",
 ]
 
 # At the bottom of settings.py
