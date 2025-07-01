c5bdab60e535f2e4436a03546da4de92dad64266
Elisa Holland <102695327+emholland@users.noreply.github.com>
2025-07-01 14:32:52 -0400

    cors fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -105,6 +105,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "https://j-web.onrender.com",
 ]
 
 # At the bottom of settings.py
